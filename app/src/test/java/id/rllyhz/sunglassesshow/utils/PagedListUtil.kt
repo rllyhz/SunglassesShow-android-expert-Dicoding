e34b474add5a7a3de1af802d229273bdf96550aa
rllyhz <rullyihza00@gmail.com>
2021-06-01 03:24:26 +0700

    Updating PagedListUtil.kt file

--- app/src/test/java/id/rllyhz/sunglassesshow/utils/PagedListUtil.kt
+++ app/src/test/java/id/rllyhz/sunglassesshow/utils/PagedListUtil.kt
@@ -10,12 +10,7 @@ object PagedListUtil {
     @Suppress("UNCHECKED_CAST")
     fun <T : Any> mockPagedList(list: List<T>): PagedList<T> {
         val pagedList = Mockito.mock(PagedList::class.java) as PagedList<T>
-        `when`(pagedList[anyInt()]).then { invocation ->
-            val index = invocation.arguments.first() as Int
-            list[index]
-        }
         `when`(pagedList.size).thenReturn(list.size)
-
         return pagedList
     }
 }
