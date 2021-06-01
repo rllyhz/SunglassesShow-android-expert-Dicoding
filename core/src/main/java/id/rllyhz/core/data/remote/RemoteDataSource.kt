d9a50f97bd4a86b8cf42d76da8f08725279094c1
rllyhz <rullyihza00@gmail.com>
2021-06-01 17:59:05 +0700

    Finishing up testing

--- core/src/main/java/id/rllyhz/core/data/remote/RemoteDataSource.kt
+++ core/src/main/java/id/rllyhz/core/data/remote/RemoteDataSource.kt
@@ -2,7 +2,6 @@ package id.rllyhz.core.data.remote
 
 import id.rllyhz.core.api.ApiEndpoint
 import id.rllyhz.core.utils.ext.asModels
-import id.rllyhz.core.utils.testing.EspressoIdlingResource
 import id.rllyhz.core.vo.ApiResponse
 import id.rllyhz.core.vo.Resource
 import kotlinx.coroutines.Dispatchers
@@ -15,8 +14,6 @@ class RemoteDataSource @Inject constructor(
 ) {
 
     suspend fun getAllMovies() = flow {
-        EspressoIdlingResource.increment()
-
         try {
             val callResults = api.discoverMovies()
             val data = callResults.body()
@@ -24,24 +21,18 @@ class RemoteDataSource @Inject constructor(
             if (callResults.isSuccessful && data != null) {
                 if (data.movies.isNotEmpty()) {
                     emit(ApiResponse.Success(data.movies.asModels()))
-                    EspressoIdlingResource.decrement()
                 } else {
                     emit(ApiResponse.Empty)
-                    EspressoIdlingResource.decrement()
                 }
             } else {
                 emit(ApiResponse.Error(callResults.message()))
-                EspressoIdlingResource.decrement()
             }
         } catch (exc: Exception) {
             emit(ApiResponse.Error(exc.message ?: "Error occurred!"))
-            EspressoIdlingResource.decrement()
         }
     }.flowOn(Dispatchers.IO)
 
     suspend fun getAllTVShows() = flow {
-        EspressoIdlingResource.increment()
-
         try {
             val callResults = api.discoverTVShows()
             val data = callResults.body()
@@ -49,24 +40,18 @@ class RemoteDataSource @Inject constructor(
             if (callResults.isSuccessful && data != null) {
                 if (data.tvShows.isNotEmpty()) {
                     emit(ApiResponse.Success(data.tvShows.asModels()))
-                    EspressoIdlingResource.decrement()
                 } else {
                     emit(ApiResponse.Empty)
-                    EspressoIdlingResource.decrement()
                 }
             } else {
                 emit(ApiResponse.Error(callResults.message()))
-                EspressoIdlingResource.decrement()
             }
         } catch (exc: Exception) {
             emit(ApiResponse.Error(exc.message ?: "Error occurred!"))
-            EspressoIdlingResource.decrement()
         }
     }.flowOn(Dispatchers.IO)
 
     suspend fun getMovieById(id: Int) = flow {
-        EspressoIdlingResource.increment()
-
         try {
             val callResults = api.getMovieDetailOf(id)
             val data = callResults.body()
@@ -74,24 +59,18 @@ class RemoteDataSource @Inject constructor(
             if (callResults.isSuccessful && callResults.code() == 200) {
                 if (data != null) {
                     emit(ApiResponse.Success(data.asModels()))
-                    EspressoIdlingResource.decrement()
                 } else {
                     emit(ApiResponse.Empty)
-                    EspressoIdlingResource.decrement()
                 }
             } else {
                 emit(ApiResponse.Error(callResults.message()))
-                EspressoIdlingResource.decrement()
             }
         } catch (exc: Exception) {
             emit(ApiResponse.Error(exc.message ?: "Error occurred!"))
-            EspressoIdlingResource.decrement()
         }
     }.flowOn(Dispatchers.IO)
 
     suspend fun getTVShowById(id: Int) = flow {
-        EspressoIdlingResource.increment()
-
         try {
             val callResults = api.getTvShowDetailOf(id)
             val data = callResults.body()
@@ -99,23 +78,18 @@ class RemoteDataSource @Inject constructor(
             if (callResults.isSuccessful && callResults.code() == 200) {
                 if (data != null) {
                     emit(ApiResponse.Success(data.asModels()))
-                    EspressoIdlingResource.decrement()
                 } else {
                     emit(ApiResponse.Empty)
-                    EspressoIdlingResource.decrement()
                 }
             } else {
                 emit(ApiResponse.Error(callResults.message()))
-                EspressoIdlingResource.decrement()
             }
         } catch (exc: Exception) {
             emit(ApiResponse.Error(exc.message ?: "Error occurred!"))
-            EspressoIdlingResource.decrement()
         }
     }.flowOn(Dispatchers.IO)
 
     fun getSimilarMoviesOfMovie(id: Int) = flow {
-        EspressoIdlingResource.increment()
         emit(Resource.loading(null))
 
         try {
@@ -124,19 +98,15 @@ class RemoteDataSource @Inject constructor(
 
             if (callResults.isSuccessful && data != null) {
                 emit(Resource.success(data.movies.asModels()))
-                EspressoIdlingResource.decrement()
             } else {
                 emit(Resource.error(null, callResults.message()))
-                EspressoIdlingResource.decrement()
             }
         } catch (exc: Exception) {
             emit(Resource.error(null, exc.message ?: "Error occurred!"))
-            EspressoIdlingResource.decrement()
         }
     }.flowOn(Dispatchers.IO)
 
     fun getSimilarTVShowsOfMovie(id: Int) = flow {
-        EspressoIdlingResource.increment()
         emit(Resource.loading(null))
 
         try {
@@ -145,19 +115,15 @@ class RemoteDataSource @Inject constructor(
 
             if (callResults.isSuccessful && data != null) {
                 emit(Resource.success(data.tvShows.asModels()))
-                EspressoIdlingResource.decrement()
             } else {
                 emit(Resource.error(null, callResults.message()))
-                EspressoIdlingResource.decrement()
             }
         } catch (exc: Exception) {
             emit(Resource.error(null, exc.message ?: "Error occurred!"))
-            EspressoIdlingResource.decrement()
         }
     }.flowOn(Dispatchers.IO)
 
     fun searchMovies(query: String) = flow {
-        EspressoIdlingResource.increment()
         emit(Resource.loading(null))
 
         try {
@@ -166,19 +132,15 @@ class RemoteDataSource @Inject constructor(
 
             if (callResults.isSuccessful && data != null) {
                 emit(Resource.success(data.movies.asModels()))
-                EspressoIdlingResource.decrement()
             } else {
                 emit(Resource.error(null, callResults.message()))
-                EspressoIdlingResource.decrement()
             }
         } catch (exc: Exception) {
             emit(Resource.error(null, exc.message ?: "Error occurred!"))
-            EspressoIdlingResource.decrement()
         }
     }.flowOn(Dispatchers.IO)
 
     fun searchTVShows(query: String) = flow {
-        EspressoIdlingResource.increment()
         emit(Resource.loading(null))
 
         try {
@@ -187,14 +149,11 @@ class RemoteDataSource @Inject constructor(
 
             if (callResults.isSuccessful && data != null) {
                 emit(Resource.success(data.tvShows.asModels()))
-                EspressoIdlingResource.decrement()
             } else {
                 emit(Resource.error(null, callResults.message()))
-                EspressoIdlingResource.decrement()
             }
         } catch (exc: Exception) {
             emit(Resource.error(null, exc.message ?: "Error occurred!"))
-            EspressoIdlingResource.decrement()
         }
     }.flowOn(Dispatchers.IO)
 }
