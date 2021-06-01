4f85d5fb7c4bc29f94fc3a8069615c4018a38c00
rllyhz <rullyihza00@gmail.com>
2021-06-01 16:18:28 +0700

    Implementing EspressoIdlingResource into remote data source

--- core/src/main/java/id/rllyhz/core/utils/testing/EspressoIdlingResource.kt
+++ core/src/main/java/id/rllyhz/core/utils/testing/EspressoIdlingResource.kt
@@ -1,10 +1,10 @@
-package id.rllyhz.sunglassesshow.utils.testing
+package id.rllyhz.core.utils.testing
 
 import androidx.test.espresso.idling.CountingIdlingResource
 
 object EspressoIdlingResource {
     private const val RESOURCE = "GLOBAL"
-    val espressoIdlingResource = CountingIdlingResource(RESOURCE)
+    private val espressoIdlingResource = CountingIdlingResource(RESOURCE)
 
     fun increment() {
         espressoIdlingResource.increment()
